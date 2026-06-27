d72b38e882e92c4efbb917855c82275ab64666fe
delavegavolochenlucas <delavegavolochenlucas@gmail.com>
2026-06-27 17:21:26 -0300

    Delete variables_dril.py

--- variables_dril.py
+++
@@ -1,9 +0,0 @@
-# A variables drill.
-startup_name = "idk"   # startup_name value is set as "idk".
-founder_name = "Lucas"   # founder_name is set as "Lucas".
-current_capital = 0   # current_capital is set as 0.
-market_share = 0   # market_share is set as 0.
-print(f"The startup name is {startup_name}")   # Prints the message: The startup name is idk. 'idk' is the value of the variable "startup_name".
-print(f"The founder name is {founder_name}")   # Prints the message: The founder name is Lucas. 'Lucas' is the value of the variable "founder_name".
-print(f"Their current capital is {current_capital}")   # Prints the message: Their current capital is 0. '0' is the value of the variable "current_capital".
-print(f"Their current market share is {market_share}")   # Prints the message: Their current market share is 0. '0' is the value of the variable "market_share".
